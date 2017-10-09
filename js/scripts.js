4600c0071f30eac7b22df5d9ed35b92ca16f37fe
espinozamaria <phillyswirl002@mail.fresnostate.edu>
2017-10-09 16:05:15 -0700

    Added some basic styling to the hw

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,26 @@ The below script searches reddit’s current top five posts on cats. Take these 
 
 Extra credit: Add a search bar where you can update the search term from cats to anything you want.
 */
-getReq('https://www.reddit.com/r/php/search.json?q=cats&limit=5', processAjax);
+//
+var url = "cats";
+var newurl = 'https://www.reddit.com/r/php/search.json?q=' + url + '&limit=5';
+// // function urlname(){
+// //   if(newurl ==  )
+// // }
+//
+// // window.onload = function()
+// // {
+// //   catresults();
+// // }
+// function catresults(){
+//     getReq(newurl, processAjax);
+// }
+getReq(newurl, processAjax);
 
 // Get a reference to the element, before we want to insert the element
 var pTag = document.getElementById("child");
 // Get a reference to the parent element
 var parent = pTag.parentNode;
-
 const newPosts = [];
 function getReq(url, callback){
     var req = new XMLHttpRequest();
@@ -43,15 +56,22 @@ function getPostInfo(postObject) {
 function loadData() {
 	newPosts.forEach(post => {
     var li1 = document.createElement('li');
-    li1.id = 'listItemID';
     li1.innerHTML = post.title;
     var li2 = document.createElement('a');
     li2.setAttribute('target', '_blank');
-    li2.id = 'listItem';
     li2.href = post.url;
     li2.innerHTML = post.url;
     parent.appendChild(li1);
     parent.appendChild(li2);
-    console.log(post.title);
+    //console.log(post.title);
 	})
 }
+
+// function newsearch(){
+//   console.log("inside the newsearch() function");
+//   var item = document.getElementById("intext").value;
+//   console.log(document.getElementById("intext").value);
+//   newurl = 'https://www.reddit.com/r/php/search.json?q=' + item + '&limit=5';
+//   getReq(newurl, processAjax);
+//   console.log(newurl);
+// }
